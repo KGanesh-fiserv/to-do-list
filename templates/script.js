3308b706c41cbaf98aa942eddcb035aa8677d959
Ganesh <kirtana.ganesh@fiserv.com>
2023-12-04 11:12:48 -0500

    Completed to-do list with dark-mode

--- templates/script.js
+++ templates/script.js
@@ -1,17 +1,9 @@
 
 //counters
-var num = 1
+var num = 0
 //eventListners
 document.getElementById("add").addEventListener("click",addTask)
 document.getElementById('dark mode').addEventListener("click", darkMode)
-var editButtons = document.querySelectorAll(".edit");
-var deleteButtons = document.querySelectorAll(".delete");
-for(let i = 0; i<editButtons.length; i++){
-    editButtons[i].addEventListener("click", buttonEditPressed)
-}
-for(let i = 0; i<deleteButtons.length; i++){
-    deleteButtons[i].addEventListener("click", buttonDeletePressed)
-}
 document.getElementById("clear").addEventListener("click", clearTasks)
 
 //Create button
@@ -33,32 +25,10 @@ function createButtons(taskInput){
     deleteButton.innerText= "Delete";
     deleteButton.addEventListener("click", buttonDeletePressed)
 
-    var completeButton = document.createElement("button");
-    completeButton.className = "complete";
-    completeButton.innerText= "Complete";
-    completeButton.addEventListener("click", buttonCompletePressed)
-    return [labelVal,editVal, editButton, deleteButton, completeButton];
-}
-function createButtonsCompletedEdition(taskInput){
-    var labelVal = document.createElement("label");
-    console.log(taskInput)
-    labelVal.innerText = taskInput;
-    console.dir(labelVal)
-
-    var editVal = document.createElement("input")
-    editVal.type = "text";
-    editVal.classList.add('show');
-
-    var editButton = document.createElement("button");
-    editButton.className = "edit";
-    editButton.innerText= "Edit";
-    editButton.addEventListener("click", buttonEditPressed)
-
-    var deleteButton= document.createElement("button");
-    deleteButton.className = "delete";
-    deleteButton.innerText= "Delete";
-    deleteButton.addEventListener("click", buttonDeletePressed)
-    return [labelVal,editVal, editButton, deleteButton];
+    var check = document.createElement("input");
+    check.type = "checkbox";
+    check.addEventListener("change", buttonCompletePressed)
+    return [check,labelVal,editVal,editButton, deleteButton];
 }
 //button handler
 function buttonEditPressed(e){
@@ -100,32 +70,38 @@ function addTask() {
 }
 //edit task
 function editTask(id){
-   listItem = document.getElementById(id);
-   console.dir(listItem)
-   let editInput = listItem.querySelector("input[type=text]");
-   let text = listItem.innerText.split("\n");
-   text[0] = editInput.value
-   listItem.innerText = text.join('\n')
- 
+    listItem = document.getElementById(id);
+    let editInput = listItem.querySelector("input[type=text]");
+    let labelVal = listItem.querySelector("label");
+    let containsClass = listItem.classList.contains("editMode");
+    if (containsClass){
+        labelVal.innerText = editInput.value;
+    }
+    else{
+        editInput.value = listItem.firstChild.innerText;
+    }
+    listItem.classList.toggle("editMode");
+   
 
 }
+
 //delete task
 function deleteTask(id){
     document.getElementById(id).remove();
 }
 //complete task
 function completeTask(id){
-    let li = document.createElement("li");
-    let completeTasks= document.getElementById("completed-tasks");
-    var txt  = document.getElementById(id).querySelector("label").innerText;
-    var buttonsList = createButtonsCompletedEdition(txt)
-        for(let i=0; i<buttonsList.length;i++){
-            li.appendChild(buttonsList[i]);
-        }
-    li.id = num;
-    num++;
-    completeTasks.appendChild(li);
-    deleteTask(id);
+    var listItem = document.getElementById(id);
+
+    if(listItem.querySelector("input[type=checkbox]").checked){
+        let completedTasks= document.getElementById("completed-tasks");
+        completedTasks.appendChild(listItem);
+        
+    }
+    else{
+        let incompleteTasks= document.getElementById("incomplete-tasks");
+        incompleteTasks.appendChild(listItem);
+    }
 
 }
 //clear
@@ -141,5 +117,6 @@ function clearTasks(){
 }
 function darkMode(){
     var element = document.body;
-    element.classList.toggle("dark-mode")
+    element.classList.toggle("dark-mode");
+
 }
